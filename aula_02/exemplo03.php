e76114b0a22a3f9686221d0269bc42f39bba76dd
vrodrigueskauan <kauanvenanciobarata@gmail.com>
2025-02-25 12:09:27 -0300

    aa

---
+++ aula_02/exemplo03.php
@@ -0,0 +1,17 @@
+<?php
+    $vetor = ['kauan', 'sarah', 'apolo', 'tomtom','popoca','saori','reizinho','naninha'];
+
+    $qtde = count($vetor);
+
+    //echo $qtde;
+    //var_dump($vetor);
+    //echo "<br>";
+    //echo $vetor[1]; 
+
+
+    for ($i=0; $i <  $qtde; $i++) { 
+        echo $vetor[$i];
+        echo "<br>";
+    }
+
+?>
